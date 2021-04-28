a0b6c28c87cf5a013376884cdd8dad82ada2120e
Vaishnavi Yelisetti <18wh1a0567@bvrithyderabad.edu.in>
2021-04-28 15:21:32 +0530

    first commit

---
+++ 18WH1A0567_Program1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+probStudAbsentAndFriday = 0.03
+probFriday = 0.2
+
+
+# In[5]:
+
+
+bayesResult = ( probStudAbsentAndFriday / probFriday )
+print(bayesResult * 100)
+
+
+# In[ ]:
+
+
+
+
